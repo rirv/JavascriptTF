10ee42dfb35c9a6ece313f95ff0172319e44214d
Rodrigo Rideout <rodrigorideout@gmail.com>
2022-11-14 21:51:25 -0300

    se agrega array

--- index.js
+++ index.js
@@ -26,6 +26,55 @@ function cuotas() {
   alert(`El total de su compra es ${totalCarro} y su forma de pago es ${pago}`);
 }
 
+const productosExistenes = [];
+
+class Producto {
+  constructor(numero, nombre, precio) {
+    this.numero = numero;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+const frutillas = new Producto(1, "Frutillas", 200);
+productosExistenes.push(frutillas);
+const lechugas = new Producto(2, "Lechugas", 100);
+productosExistenes.push(lechugas);
+const tomates = new Producto(3, "Tomates", 150);
+productosExistenes.push(tomates);
+const manzanas = new Producto(4, "Manzanas", 50);
+productosExistenes.push(manzanas);
+const yerba = new Producto(5, "Yerba Mate", 200);
+productosExistenes.push(yerba);
+const te = new Producto(6, "Te", 100);
+productosExistenes.push(te);
+const harina = new Producto(7, "Harina", 150);
+productosExistenes.push(harina);
+const dulce = new Producto(8, "Dulce de Membrillo", 50);
+productosExistenes.push(dulce);
+const jabon = new Producto(9, "Jabon", 200);
+productosExistenes.push(jabon);
+const shampoo = new Producto(10, "Shampoo", 100);
+productosExistenes.push(shampoo);
+const desodorante = new Producto(11, "Desodorante", 150);
+productosExistenes.push(desodorante);
+const dentifrico = new Producto(12, "Dentifrico", 50);
+productosExistenes.push(dentifrico);
+const vino = new Producto(13, "Vino", 200);
+productosExistenes.push(vino);
+const cerveza = new Producto(14, "Cerveza", 100);
+productosExistenes.push(cerveza);
+const jugo = new Producto(15, "Jugo", 150);
+productosExistenes.push(jugo);
+const tehelado = new Producto(16, "Te Helado", 50);
+productosExistenes.push(tehelado);
+console.log(productosExistenes);
+
+const articulos = productosExistenes.filter((frutas) => frutas === frutillas );
+console.log(articulos)
+
+
+
+
 let rubro = parseInt(
   prompt(
     "Selecione el rubro que desea comprar: 1.Frutas/Verduras 2.Almacen 3.Aseo personal 4.Bebidas"
@@ -37,14 +86,11 @@ while (seguirComprando === true) {
     let producto1 = parseInt(
       prompt("Seleccione 1.Frutillas 2. Lechugas 3.Tomates 4.Manzanas")
     );
-    if (producto1 === 1) {
-      totalCarro = totalCarro + 200;
-    } else if (producto1 === 2) {
-      totalCarro = totalCarro + 100;
-    } else if (producto1 === 3) {
-      totalCarro = totalCarro + 150;
-    } else if (producto1 === 4) {
-      totalCarro = totalCarro + 50;
+    const productoEscogido1 = productosExistenes.find(
+      (producto) => producto.numero === producto1
+    );
+    if (productoEscogido1) {
+      totalCarro = totalCarro + productoEscogido1.precio;
     } else {
       rubro = parseInt(
         prompt(
@@ -55,16 +101,13 @@ while (seguirComprando === true) {
     }
   } else if (rubro === 2) {
     let producto2 = parseInt(
-      prompt("Seleccione 1.Yerba Mate 2.Te 3.Harina 4.Dulce de Membrillo")
+      prompt("Seleccione 5.Yerba Mate 6.Te 7.Harina 8.Dulce de Membrillo")
     );
-    if (producto2 === 1) {
-      totalCarro = totalCarro + 200;
-    } else if (producto2 === 2) {
-      totalCarro = totalCarro + 100;
-    } else if (producto2 === 3) {
-      totalCarro = totalCarro + 150;
-    } else if (producto2 === 4) {
-      totalCarro = totalCarro + 50;
+    const productoEscogido2 = productosExistenes.find(
+      (producto) => producto.numero === producto2
+    );
+    if (productoEscogido2) {
+      totalCarro = totalCarro + productoEscogido2.precio;
     } else {
       rubro = parseInt(
         prompt(
@@ -75,16 +118,13 @@ while (seguirComprando === true) {
     }
   } else if (rubro === 3) {
     let producto3 = parseInt(
-      prompt("Seleccione 1.Jabon 2.Shampoo 3.Desodorante 4.Dentifrico")
+      prompt("Seleccione 9.Jabon 10.Shampoo 11.Desodorante 12.Dentifrico")
     );
-    if (producto3 === 1) {
-      totalCarro = totalCarro + 200;
-    } else if (producto3 === 2) {
-      totalCarro = totalCarro + 100;
-    } else if (producto3 === 3) {
-      totalCarro = totalCarro + 150;
-    } else if (producto3 === 4) {
-      totalCarro = totalCarro + 50;
+    const productoEscogido3 = productosExistenes.find(
+      (producto) => producto.numero === producto3
+    );
+    if (productoEscogido3) {
+      totalCarro = totalCarro + productoEscogido3.precio;
     } else {
       rubro = parseInt(
         prompt(
@@ -95,16 +135,13 @@ while (seguirComprando === true) {
     }
   } else if (rubro === 4) {
     let producto4 = parseInt(
-      prompt("Seleccione 1.Vino 2.Cerveza 3.Jugos 4.Te Helado")
+      prompt("Seleccione 13.Vino 14.Cerveza 15.Jugos 16.Te Helado")
     );
-    if (producto4 === 1) {
-      totalCarro = totalCarro + 200;
-    } else if (producto4 === 2) {
-      totalCarro = totalCarro + 100;
-    } else if (producto4 === 3) {
-      totalCarro = totalCarro + 150;
-    } else if (producto4 === 4) {
-      totalCarro = totalCarro + 50;
+    const productoEscogido4 = productosExistenes.find(
+      (producto) => producto.numero === producto4
+    );
+    if (productoEscogido4) {
+      totalCarro = totalCarro + productoEscogido4.precio;
     } else {
       producto4 = parseInt(
         prompt(
